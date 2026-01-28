ad9c31c5d9acb7885d437a5152bfa60f784ea40e
BAPTISTE ROYER <broyer@freedommobile.ca>
2026-01-28 16:15:17 +0100

    Ajout du README.md

--- src/main/resources/db/migration/V1__initialisation.sql
+++ src/main/resources/db/migration/V1__initialisation.sql
@@ -1,3 +1,8 @@
+-- Flyway effectue les migrations AVANT que Hibernate puisse créer les tables !
+-- De ce fait, je suis obligé de créer ici les tables.
+-- Cependant, Hibernate ne vas pas agir comme le créateur des tables, mais comme un outil de vérification.
+-- Il doit vérifier si les tables fournis par Flyway correspondent aux siennes.
+
 -- USERS TABLE
 CREATE TABLE users (
     cid INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,3 +29,10 @@ CREATE TABLE appointment (
 );
 
 INSERT INTO users(cid, address, admin, email, first_name, last_name, password, pfp_id, role) VALUES (0, '48 rue Clarisse, Haubourdin', TRUE, 'baptiste-royer@outlook.com', 'Baptiste', 'Royer', '$2a$10$oy9yJ9Ayi4628w1rBgGNWewWBfQBgRhhbRwYLKIpbxGgpeEjI8J.O', 'default.png', 'client');
+
+INSERT INTO users(cid, address, admin, email, first_name, last_name, password, pfp_id, role) VALUES (1, '48 rue Clarisse, Haubourdin', FALSE, 'baptiste-royer@outlook.com', 'Jai', 'Je', '$2a$10$oy9yJ9Ayi4628w1rBgGNWewWBfQBgRhhbRwYLKIpbxGgpeEjI8J.O', 'default.png', 'client');
+INSERT INTO users(cid, address, admin, email, first_name, last_name, password, pfp_id, role) VALUES (2, '48 rue Clarisse, Haubourdin', FALSE, 'baptiste-royer@outlook.com', 'Pas', 'Suis', '$2a$10$oy9yJ9Ayi4628w1rBgGNWewWBfQBgRhhbRwYLKIpbxGgpeEjI8J.O', 'default.png', 'client');
+INSERT INTO users(cid, address, admin, email, first_name, last_name, password, pfp_id, role) VALUES (3, '48 rue Clarisse, Haubourdin', FALSE, 'baptiste-royer@outlook.com', 'Didee', 'A', '$2a$10$oy9yJ9Ayi4628w1rBgGNWewWBfQBgRhhbRwYLKIpbxGgpeEjI8J.O', 'default.png', 'client');
+INSERT INTO users(cid, address, admin, email, first_name, last_name, password, pfp_id, role) VALUES (4, '48 rue Clarisse, Haubourdin', FALSE, 'baptiste-royer@outlook.com', 'Desole', 'Sec', '$2a$10$oy9yJ9Ayi4628w1rBgGNWewWBfQBgRhhbRwYLKIpbxGgpeEjI8J.O', 'default.png', 'client');
+
+INSERT INTO appointment(appointment_date, appointment_time, cid) VALUES ('2026-10-21', '08:30:00', 2)
